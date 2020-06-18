946cf8c7bcc94fb113cb4463c910027c89559cca
Yamada Koki <lenny.srv.826@gmail.com>
2020-06-19 07:12:50 +0900

    Update : メニューの開閉状態

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,31 @@
       color="primary"
       dark
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="openSideMenu"></v-app-bar-nav-icon>
       <v-toolbar-title>マイアドレス帳</v-toolbar-title>
       <v-spacer></v-spacer>
     </v-app-bar>
 
     <v-content>
+      <SideNav/>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import SideNav from './components/SideNav'
 export default {
   name: 'App',
-
+  components: {
+    SideNav
+  },
   data: () => ({
     //
   }),
+  methods: {
+    openSideMenu () {
+      this.$store.dispatch('toggleSideMenu')
+    }
+  }
 };
 </script>
