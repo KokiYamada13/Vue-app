87355300882d1356384ba5d13baf6d9105aec278
Yamada Koki <lenny.srv.826@gmail.com>
2020-06-19 07:43:13 +0900

    Add:  一覧ページ作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Addresses from '../views/Addresses.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/addresses',
+    name: 'addresses',
+    component: Addresses
   },
   {
     path: '/about',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="home">
-    <Addresses/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Addresses from '@/components/Addresses.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    Addresses
-  }
+  name: 'Home'
 }
 </script>
