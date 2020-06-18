03c12160004951ef2efae1e536cc55019830dadd
Yamada Koki <lenny.srv.826@gmail.com>
2020-06-19 06:09:37 +0900

    1st commit

---
+++ src/components/Addresses.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>アドレス帳</h1>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scored lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Addresses/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Addresses from '@/components/Addresses.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Addresses
   }
 }
 </script>
